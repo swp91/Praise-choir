59d2acfe4fcb66b59d46fa13bdbd25ca3dbb70f9
seongwoo <seongwoo4477@gmail.com>
2026-06-23 08:46:16 +0900

    chore: SEO 최적화를 위해 sitemap 및 robots.txt의 도메인을 www 버전으로 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/admin/',
     },
-    sitemap: 'https://praisechoir.kr/sitemap.xml',
+    sitemap: 'https://www.praisechoir.kr/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://praisechoir.kr';
+  const baseUrl = 'https://www.praisechoir.kr';
   
   const routes = [
     '',
